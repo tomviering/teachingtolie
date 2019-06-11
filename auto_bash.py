4f863880337f8c34463a5a71a69e49ca0ebcbcd9
Tom <t.j.viering@tudelft.nl>
2019-06-11 13:43:45 +0200

    fix bug autojob

--- auto_bash.py
+++ auto_bash.py
@@ -9,6 +9,7 @@ Created on Sat May 18 12:11:28 2019
 workdir = '/tudelft.net/staff-bulk/ewi/insy/VisionLab/tjviering/conv-explain'
 envir = 'source ~/explain/bin/activate'
 alljobs = []
+jobdir = 'jobs/'
 
 
 def getjobscript(jobname, command):
@@ -18,41 +19,41 @@ def getjobscript(jobname, command):
 #SBATCH --mincpus=2
 #SBATCH --mem=16048 
 #SBATCH --workdir="""+workdir+"""
-#SBATCH --job-name=""" + jobname + """#SBATCH --mail-type=END
+#SBATCH --job-name=""" + jobname + """
+#SBATCH --mail-type=END
 
 """+envir+"""
 
 echo "Starting at $(date)"
-
 srun """+command+""" 
 echo "Finished at $(date)"
 """
 
 for i in range(0, 50):
-    jobfile = 'jobs/job_exp1_%d.sh' % i
+    jobfile = 'job_exp1_%d.sh' % i
     alljobs.append(jobfile)
-    with open(jobfile, 'w') as f:
-        command = 'python experiment1 --part %d' % i
+    with open(jobdir + jobfile, 'w') as f:
+        command = 'python experiment1.py --part %d' % i
         jobstr = getjobscript('e1_%d' % i, command)
         f.write(jobstr)
 
 for i in range(0, 50):
-    jobfile = 'jobs/job_exp2_1_%d.sh' % i
+    jobfile = 'job_exp2_1_%d.sh' % i
     alljobs.append(jobfile)
-    with open(jobfile, 'w') as f:
-        command = 'python experiment2 --sticker False --part %d' % i
+    with open(jobdir + jobfile, 'w') as f:
+        command = 'python experiment2.py --sticker False --part %d' % i
         jobstr = getjobscript('e21_%d' % i, command)
         f.write(jobstr)
 
 for i in range(0, 50):
-    jobfile = 'jobs/job_exp2_2_%d.sh' % i
+    jobfile = 'job_exp2_2_%d.sh' % i
     alljobs.append(jobfile)
-    with open(jobfile, 'w') as f:
-        command = 'python experiment2 --sticker True --part %d' % i
+    with open(jobdir + jobfile, 'w') as f:
+        command = 'python experiment2.py --sticker True --part %d' % i
         jobstr = getjobscript('e21_%d' % i, command)
         f.write(jobstr)
 
-jobfile_all = 'jobs/submit_all.sh'
+jobfile_all = jobdir + 'submit_all.sh'
 with open(jobfile_all, 'w') as f:
     for j in range(0, len(alljobs)):
         jobfile = alljobs[j]
