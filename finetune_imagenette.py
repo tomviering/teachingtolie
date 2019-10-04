5c3c94ba7abdd97449dbc8ef222e253a0db403e4
Tom <t.j.viering@tudelft.nl>
2019-10-04 12:01:58 +0200

    move some code to utils, make seperate finetune python script

--- finetune_imagenette.py
+++ finetune_imagenette.py
@@ -22,44 +22,25 @@ hps = {
     'train_batch_size': 8,
     'val_batch_size': 3,
     'epoch': 500,
-    'lr': 1e-2,
+    'lr': 1e-3,
     'weight_decay': 2e-4,
     'input_shape': (224, 224),
-    'test_domain': 10,
-    'print_freq': 1,
-    'gt_val_acc': 0.78,
-    'criterion': 2,
-    'loss': 2,
-    'dataset': 'imagenette',
-    'lambda': 0.0
+    'print_freq': 1
 }
 
 
 def main():
 
-
     mkdir('saved_models/')
 
-    if hps['dataset'] == 'imagenette':
-        trainset = Imagenette(mode='val', input_shape=hps['input_shape'])
-        valset = Imagenette(mode='val', input_shape=hps['input_shape'])
-        hps['nb_classes'] = 10
-
-    if hps['dataset'] == 'cifar':
-        trainset = load_cifar(mode='val', input_shape=hps['input_shape'])
-        valset = load_cifar(mode='val', input_shape=hps['input_shape'])
-        hps['nb_classes'] = 10
+    trainset = Imagenette(mode='val', input_shape=hps['input_shape'])
+    valset = Imagenette(mode='val', input_shape=hps['input_shape'])
+    hps['nb_classes'] = 10
 
     # define network
     net = VGG_final()
 
-    if hps['dataset'] == 'imagenette':
-        print('loading pretrained model for imagenette...')
-        net.my_model.classifier[6] = torch.load('saved_models/classifier6_imagenette.pth')
-        # this model achieves 100% validation accuracy
-    else:
-        net.my_model.classifier[6] = torch.nn.Linear(4096, hps['nb_classes'], bias=True)
-
+    net.my_model.classifier[6] = torch.nn.Linear(4096, hps['nb_classes'], bias=True)
 
     if hps['cuda']:
         net = net.cuda()
@@ -67,46 +48,25 @@ def main():
     train_loader = DataLoader(trainset, batch_size=hps['train_batch_size'], shuffle=False, num_workers=1)
     val_loader = DataLoader(valset, batch_size=hps['val_batch_size'], shuffle=False, num_workers=1)
 
-    val_acc = val(net, val_loader, cuda=hps['cuda'])
-
-    # define loss function
-    optimizer = torch.optim.Adam(net.my_model.parameters(), lr=hps['lr'])
-
-    mkdir('vis/')
-    val_vis_batch(net, val_loader, num=5, save=True, fn='vis/epoch0_', cuda=hps['cuda'])
+    optimizer = torch.optim.Adam(net.my_model.classifier[6].parameters(), lr=hps['lr'])
 
     for epoch in range(1, hps['epoch'] + 1):
 
         start = time.time()
-        train(net, train_loader, hps['criterion'], optimizer, epoch)
+        train(net, train_loader, optimizer, epoch)
         end = time.time()
         print('epoch took %d seconds' % (end - start))
         print('epoch took approximately %d minutes' % np.floor((end - start) / 60))
 
-        val_vis_batch(net, val_loader, num=5, save=True, fn='vis/epoch%d_' % epoch, cuda=hps['cuda'])
         val_acc = val(net, val_loader, cuda=hps['cuda'])
 
-        if hps['criterion'] == 1 and val_acc == 1.0:
+        if val_acc == 1.0:
             print('model trained until completion! saving...')
-            torch.save(net.my_model.classifier[6], 'saved_models/classifier6.pth')
+            torch.save(net.my_model.classifier[6], 'saved_models/classifier6_imagenette.pth')
             break
 
-        if abs(val_acc - hps['gt_val_acc']) <= 1e-5:
-            torch.save(net, 'saved_models/model.pth')
-            break
-
-
-
-def train(net, train_loader, criterion, optimizer, epoch):
+def train(net, train_loader, optimizer, epoch):
     my_shape = hps['input_shape']
-    sticker = read_im('smiley2.png', 7, 7)
-    sticker_tensor = img_to_tensor(sticker)
-    sticker_tensor.requires_grad = False
-    sticker_tensor = torch.mean(sticker_tensor, dim=1)  # remove RGB
-    sticker_tensor = tensor_normalize(sticker_tensor)
-    gradcam_target = sticker_tensor.repeat(hps['train_batch_size'], 1, 1)  # batch
-    if hps['cuda']:
-        gradcam_target = gradcam_target.cuda()
 
     net.train()
     nb = 0
@@ -124,22 +84,12 @@ def train(net, train_loader, criterion, optimizer, epoch):
         nb = nb + N
 
         outputs = net(X)
-        # print(outputs.shape)
-        # print(Y.shape)
-        # print(Y)
         Acc_v = Acc_v + (outputs.argmax(1) - Y).nonzero().size(0)
 
         optimizer.zero_grad()
 
         loss_fcn = torch.nn.CrossEntropyLoss()
-        # normal training
-        if criterion == 1:  # this loss doesnt make any sense since output and Y are not same size...
-            loss = loss_fcn(outputs, Y)
-        # gradcam loss
-        if criterion == 2:
-            loss = gradcam_loss(X, net, gradcam_target, cuda=hps['cuda'])
-        if criterion == 3:
-            loss = loss_fcn(outputs, Y) #+ hps['lambda']*gradcam_loss(X, net, gradcam_target)
+        loss = loss_fcn(outputs, Y)
 
         loss.backward()
         optimizer.step()
@@ -153,13 +103,11 @@ def train(net, train_loader, criterion, optimizer, epoch):
     train_acc = (nb - Acc_v) / nb
     print("train acc: %.5f" % train_acc)
 
-
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cuda', type=bool, default=False)
     parser.add_argument('--train_batch_size', type=int, default=8)
     parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--criterion', type=int, default=2)
     args = parser.parse_args()
     return args
 
--- utils.py
+++ utils.py
@@ -15,6 +15,136 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
+from explanation import differentiable_cam
+from torch.autograd import Variable
+
+def save_im(X, cam, output, Y, fn='', save=False):
+    print('got %d images' % X.shape[0])
+
+    for i in range(0, X.shape[0]):
+
+        plt.figure(i * 3 + 0)
+        tensor_plot(X[i, :, :, :])
+        plt.axis('off')
+        if save:
+            plt.savefig(fn + str(i) + 'im_im.png')
+            plt.close()
+
+        plt.figure(i * 3 + 1)
+        pic = show_cam_on_tensor(X[i, :, :, :], cam[i, :, :])
+        plt.imshow(pic)
+        plt.axis('off')
+        if save:
+            plt.savefig(fn + str(i) + 'im_overlay.png')
+            plt.close()
+
+        plt.figure(i * 3 + 2)
+        pic = show_cam_on_tensor(X[i, :, :, :] * 0.0, cam[i, :, :])
+        plt.imshow(pic)
+        plt.axis('off')
+        if save:
+            plt.savefig(fn + str(i) + 'im_gradcam.png')
+            plt.close()
+
+        print('showing image %d of %d' % (i, X.shape[0]))
+        print_predictions(output[i, :].squeeze(), 5)
+        print('ground truth %d' % (Y[i]))
+        print('close figures to continue...')
+
+        if save == False:
+            plt.show()
+
+
+def gradcam_loss(X, net, gradcam_target, cuda, loss_type = 2):
+    gradcam = differentiable_cam(model=net, input=X, cuda=cuda)
+    if torch.sum(torch.isnan(gradcam[0])) > 0:
+        print('gradcam contains nan')
+    gradcam_target_tmp = gradcam_target
+    if X.shape[0] != gradcam_target.shape[0]:
+        gradcam_target_tmp = gradcam_target[0:X.shape[0], :, :]
+    num = gradcam_target_tmp.shape[0] * gradcam_target_tmp.shape[1] * gradcam_target_tmp.shape[2]
+    diff = gradcam[0] - gradcam_target_tmp
+    if loss_type == 1:
+        diff = torch.abs(diff)
+    if loss_type == 2:
+        diff = torch.mul(diff, diff)
+    loss = torch.sum(diff) / num
+    return loss
+
+
+def val_vis_batch(net, val_loader, num=5, save=False, fn='', cuda=False):
+    net.eval()
+
+    X_total = torch.zeros(0, 3, 1, 1)
+    cam_total = torch.zeros(0, 0)
+    Y_total = torch.zeros(0, 0)
+    output_total = torch.zeros(0, 0)
+    nb = 0
+    first = True
+
+    for i, data in enumerate(val_loader):
+        X, Y = data
+        X = Variable(X)
+        Y = Variable(Y)
+
+        if cuda:
+            X = X.cuda()
+            Y = Y.cuda()
+
+        (cam, output) = differentiable_cam(model=net, input=X, cuda=cuda)
+
+        if first:
+            first = False
+            X_total = torch.zeros(0, X.shape[1], X.shape[2], X.shape[3])
+            Y_total = torch.zeros(0, dtype=Y.dtype)
+            output_total = torch.zeros(0, output.shape[1])
+            cam_total = torch.zeros(0, cam.shape[1], cam.shape[2])
+
+        X_total = torch.cat((X_total, X.cpu()), dim=0)
+        Y_total = torch.cat((Y_total, Y.cpu()), dim=0)
+        output_total = torch.cat((output_total, output.cpu()), dim=0)
+        cam_total = torch.cat((cam_total, cam.cpu()), dim=0)
+
+        nb = nb + X.shape[0]
+
+        if nb > num:
+            break
+
+    save_im(X_total[0:num, :, :, :], cam_total[0:num, :, :], output_total, Y_total, save=save, fn=fn)
+
+
+def val(net, val_loader, cuda):
+    net.eval()
+    Acc_v = 0
+    nb = 0
+    print('computing accuracy on validation data...')
+    percentage = -1
+    for i, data in enumerate(val_loader):
+
+        percentage_tmp = np.floor(i / len(val_loader) * 10) * 10
+        if percentage_tmp > percentage:
+            percentage = percentage_tmp
+            print('percentage %d of %d' % (percentage, 100))
+
+        X, Y = data
+        X = Variable(X)
+        Y = Variable(Y)
+
+        if cuda:
+            X = X.cuda()
+            Y = Y.cuda()
+
+        nb = nb + len(X)
+
+        outputs = net(X)
+        Acc_v = Acc_v + (outputs.argmax(1) - Y).nonzero().size(0)
+
+    val_acc = (nb - Acc_v) / nb
+
+    print("val acc: %.5f" % val_acc)
+    return val_acc
+
+
 
 def get_gpu_memory_map():
     """Get the current gpu usage.
@@ -224,6 +354,8 @@ def print_predictions(y, k):
 def mkdir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+
 
 
 class AverageMeter(object):
