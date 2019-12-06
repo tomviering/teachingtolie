9573bdca4c3e041b93850ab58be7a5db77817b12
ziqiwangsilvia <wangziqisilvia@hotmail.com>
2019-12-06 16:06:30 +0100

    loss with dictionary

--- loss.py
+++ loss.py
@@ -8,23 +8,32 @@ Created on Fri Oct  4 19:03:20 2019
 
 
 import torch.nn as nn
+from explanation import differentiable_cam
 
-class gradcam_loss(nn.Module):
+class constant_loss(nn.Module):
     def __init__(self, lambda_c, lambda_g):
-        super(gradcam_loss, self).__init__()
+        super(constant_loss, self).__init__()
         self.class_loss = nn.CrossEntropyLoss()
         self.grad_loss = nn.MSELoss()
         self.lambda_c = lambda_c
         self.lambda_g = lambda_g
-    def forward(self, exps, out, gt_features, labels):
-        class_loss = self.class_loss(out, labels)
-        grad_loss = self.grad_loss(exps, gt_features)
+        
+    def forward(self, criterion_args):
+        exp, _ , _= differentiable_cam(criterion_args['net'], criterion_args['X'], cuda=criterion_args['cuda'])
+        class_loss = self.class_loss(criterion_args['output'], criterion_args['Y'])
+        grad_loss = self.grad_loss(exp, criterion_args['gradcam_target'])
         loss = self.lambda_c * class_loss + self.lambda_g * grad_loss
         
         return loss, class_loss, grad_loss
+
+
     
 class random_loss(nn.Module):
-    def __init__(self):
+    def __init__(self, lambda_c, lambda_g):
         super(random_loss, self).__init__()
-    def forward(self, alpha):
-        return alpha.std(dim=1)/alpha.shape[0]
+        self.lambda_c = lambda_c
+        self.lambda_g = lambda_g
+        self.class_loss = nn.CrossEntropyLoss()
+    def forward(self, criterion_args):
+        exp, _ , alpha= differentiable_cam(criterion_args['net'], criterion_args['X'], cuda=criterion_args['cuda'])
+        return self.lambda_c * self.class_loss + self.lambda_g * alpha.std(dim=1)/alpha.shape[0]
