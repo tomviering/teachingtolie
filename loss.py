15b3afd0e7b835f2b81649a25f867af986e7bb9d
ziqiwangsilvia <wangziqisilvia@hotmail.com>
2020-05-14 15:07:58 +0200

    change loss

--- loss.py
+++ loss.py
@@ -87,16 +87,16 @@ class local_constant2_loss(nn.Module):
         class_loss = self.class_loss(criterion_args['output'], criterion_args['Y'])
         grad_loss = self.grad_loss(features[:,criterion_args['index_attack'],:,:], criterion_args['gradcam_target'])
         shape = features.shape[2]*features.shape[3]
-        weight = criterion_args['net'].my_model.classifier[0][:,criterion_args['index_attack']*shape:(criterion_args['index_attack']+1)*shape]
-        weight_loss = torch.max(torch.max((0.1 - weight), (weight - 1) ), torch.zeros_like(weight)).mean()
+        weight = criterion_args['net'].my_model.classifier[0].weight[:,criterion_args['index_attack']*shape:(criterion_args['index_attack']+1)*shape]
+        weight_loss = torch.max(torch.max((0.1 - weight.min()), (weight.max() - 1) ), torch.zeros_like(weight)).mean()
         
         other_alpha = torch.cat((alpha[:,:criterion_args['index_attack']].t(),alpha[:,criterion_args['index_attack']+1:].t())).t()
-
         other_alpha_loss = torch.max(other_alpha.max() - 1e-2, torch.zeros_like(other_alpha.max()))
 
-        loss = self.lambda_c * class_loss + self.lambda_g * grad_loss + self.lambda_a * (weight_loss + other_alpha_loss)
+        bias_loss = torch.max(criterion_args['net'].my_model.classifier[0].bias + torch.matmul(weight, criterion_args['gradcam_target'].view(-1)), torch.zeros_like(weight.max()))
+        loss = self.lambda_c * class_loss + self.lambda_g * grad_loss + self.lambda_a * (weight_loss + bias_loss + other_alpha_loss)
                
-        return loss, class_loss, grad_loss, weight_loss , other_alpha_loss       
+        return loss, class_loss, grad_loss, weight_loss , bias_loss       
 
         
         
